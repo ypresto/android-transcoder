d7b31234daa22ee9fbfed316f7182c0af367d5fe
Mattia Iavarone <mat.iavarone@gmail.com>
2018-12-14 18:07:00 +0100

    Add useful WriteVideoValidator

--- lib/src/main/java/net/ypresto/androidtranscoder/validator/WriteAlwaysValidator.java
+++ lib/src/main/java/net/ypresto/androidtranscoder/validator/WriteAlwaysValidator.java
@@ -4,7 +4,7 @@ package net.ypresto.androidtranscoder.validator;
  * A {@link Validator} that always writes to target file, no matter the track status,
  * presence of tracks and so on. The output container file might be empty or unnecessary.
  */
-public class AlwaysWriteValidator implements Validator {
+public class WriteAlwaysValidator implements Validator {
 
     @Override
     public boolean validate(TrackStatus videoStatus, TrackStatus audioStatus) {
---
+++ lib/src/main/java/net/ypresto/androidtranscoder/validator/WriteVideoValidator.java
@@ -0,0 +1,20 @@
+package net.ypresto.androidtranscoder.validator;
+
+/**
+ * A {@link Validator} that gives priority to the video track.
+ * Transcoding will not happen if the video track does not need it, even if the
+ * audio track might need it.
+ */
+public class WriteVideoValidator implements Validator {
+
+    @Override
+    public boolean validate(TrackStatus videoStatus, TrackStatus audioStatus) {
+        switch (videoStatus) {
+            case ABSENT: return false;
+            case REMOVING: return true;
+            case COMPRESSING: return true;
+            case PASS_THROUGH: return false;
+        }
+        return true;
+    }
+}
